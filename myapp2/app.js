7c403fb0572bbf24173925c8e19593d156003140
Sagar-softechure <136797867+Sagar-softechure@users.noreply.github.com>
2025-07-08 18:36:14 +0530

    Your commit message here

--- myapp2/app.js
+++ myapp2/app.js
@@ -1,30 +1,17 @@
-const cors = require('cors');
 require('./db');
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
+const cors = require('cors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const createError = require('http-errors');
 const methodOverride = require('method-override');
-const http = require('http');
-const { Server } = require('socket.io');
 
-var app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:3001', // Your React app's port
-    credentials: true,
-  }
-});
+const authRouter = require('./routes/auth'); // or any routes you have
 
-var usersRouter = require('./routes/users');
-var postsRouter = require('./routes/posts');
-var commentRouter = require('./routes/comment');
+const app = express();
 
-// view engine setup
+// View engine setup (if needed)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -34,47 +21,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
+
 app.use(cors({
   origin: 'http://localhost:3001',
   credentials: true
 }));
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-// app.use('/posts', postsRouter);
-// app.use('/comments', commentRouter);
-app.use('/', authRouter);
 
-let chatHistory = [];
+// Routes
+app.use('/', authRouter); // You can add more as needed
 
-io.on('connection', (socket) => {
-  // Send chat history to new client
-  socket.emit('chat history', chatHistory);
+// 404 and error handler
+app.use((req, res, next) => next(createError(404)));
 
-  // Listen for new messages
-  socket.on('chat message', (msg) => {
-    chatHistory.push(msg);
-    io.emit('chat message', msg); // Broadcast to all clients
-  });
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-server.listen(3002, () => {
-  console.log('Socket.IO server running on http://localhost:3000');
 });
 
 module.exports = app;
--- myapp2/db.js
+++ myapp2/db.js
@@ -2,11 +2,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/myapp';
+const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/test';
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log("MongoDB Atlas connected"))
-.catch(err => console.error("MongoDB connection error:", err));
+.catch((err) => {console.error("MongoDB connection error:", err)
+  process.exit(1);
+});
